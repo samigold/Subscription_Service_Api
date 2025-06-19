da52516e5a639bcbd27e7b76708fec4eaac7c89d
Samuel Madu <info@samuelmadu.com>
2025-06-19 03:32:07 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Service');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
